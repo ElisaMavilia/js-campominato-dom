ab88c39d580c384750cf896c604e984a141a5989
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-18 16:00:49 +0100

    function generateNumber

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ button.addEventListener("click", function(){
 boxContainer.classList.remove("d-none"); 
 button.addEventListener("click", function(){    //Add a click function to refresh the play button
 boxContainer.innerHTML =""; 
-    })   
+})   
 })   
     for (let i = 1; i <= 100; i++){
        
@@ -49,3 +49,27 @@ logo.setAttribute('src',`img/logo.png`);
 logo.classList.add("logo-boolean");
 newElement.appendChild(logo);
 tmpHtml += `<img src="img/logo.png">`;
+
+/* Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: **nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba. */
+
+let randomNumber = [];
+let list = [1, 3, 5, 7, 8, 9, 11, 12, 13, 14, 16, 20, 25, 30, 64, 59];
+console.log(generateNumber(1, 16, list));
+
+function generateNumber (min, max, blacklist){
+    let found = false;
+    while (found === false){
+    
+     
+    }
+   
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
