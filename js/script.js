051c82bfbe1e0ac3ed015ee35e239bf5b0d66d6e
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-18 13:54:35 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/* L'utente clicca su un bottone che genererà una griglia di gioco quadrata. 
+Ogni cella ha un numero progressivo, da 1 a 100. 
+Ci saranno quindi 10 caselle per ognuna delle 10 righe. 
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+ */
+
+// COSA MI SERVE?
+
+// V Una funzione event listener per il bottone (per far apparire la griglia) 
+// V Un ciclo for per moltiplicare i singoli box
+// V Una funzione event listener per il clic dei singoli box
+
+
+// inizio prendendo il container con il suo #id
+
+
+// inizializzo un cilo di 100
+let button = document.getElementById("play");
+const boxContainer = document.getElementById("box-container");
+
+
+button.addEventListener("click", function(){    
+boxContainer.classList.remove("d-none"); 
+button.addEventListener("click", function(){    //Add a click function to refresh the play button
+boxContainer.innerHTML =""; 
+    })   
+})   
+    for (let i = 1; i <= 100; i++){
+       
+        let box = document.createElement("div"); //creo un div 
+        box.setAttribute("class", "single-box"); //al div assegno la classe box
+        box.innerHTML=i;
+        boxContainer.appendChild(box); // e lo rendo figlio dell'id box-container
+        // aggiungo funzione per cambiare il colore dei singoli box
+        // Creo un primo eventListener per il primo click
+       
+       box.addEventListener ("click", function(){ 
+            box.classList.add("bg-color");
+            console.log(`Hai selezionato la casella ${box.innerHTML}`);
+       
+         })
+    
+    }
+let tmpHtml ="";
+const newElement = document.getElementById("left-header");
+let logo = document.createElement("img");
+logo.innerHTML = '<img>';
+logo.setAttribute('src',`img/logo.png`);
+logo.classList.add("logo-boolean");
+newElement.appendChild(logo);
+tmpHtml += `<img src="img/logo.png">`;
